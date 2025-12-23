4f9396868e47b48c9ac253c64624a1278c7a08bd
sifat1234 <ahahmedsifat111@gmail.com>
2025-12-23 18:41:54 +0600

    Product Detail Page Build

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,8 +1,18 @@
-import { config } from 'dotenv'
-import { drizzle } from 'drizzle-orm/postgres-js'
-import postgres from 'postgres'
+import { drizzle } from 'drizzle-orm/node-postgres'
+import { Pool } from 'pg'
+import * as schema from './schema'
 
-config({ path: '.env' })
+if (!process.env.DATABASE_URL) {
+  throw new Error('DATABASE_URL is not set')
+}
 
-const client = postgres(process.env.DATABASE_URL!)
-export const db = drizzle({ client })
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.DATABASE_URL.includes('supabase')
+    ? {
+        rejectUnauthorized: false,
+      }
+    : false,
+})
+
+export const db = drizzle(pool, { schema })
